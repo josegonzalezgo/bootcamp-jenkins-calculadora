706eba5b044105e63ef3731860dbba7023106c13
josegonzalezgo <mamay.zak@hotmail.com>
2024-10-29 16:17:48 -0600

    Update CaluladoraImpl.java
    corregimos cambio para que pase prueba

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -6,7 +6,7 @@ public class CaluladoraImpl implements Calculadora {
 	public int calcular(int i1, char op, int i2) {
 		switch(op){
 			case '+':
-				return i1-i2;
+				return i1+i2;
 			case '-':
 				return i1-i2;
 			default:
